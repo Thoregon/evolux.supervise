29a30f67e78d65b0a5ac6697e7da8314a312660e
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-11-25 10:29:50 +0100

    common logging

--- index.mjs
+++ index.mjs
@@ -4,6 +4,8 @@
  * @author: blukassen
  */
 
-export { default as EError } from './lib/error/eerror.mjs';
+export { default as EError }    from './lib/error/eerror.mjs';
+export { emsg }                 from './lib/error/util.mjs';
+export { default as Reporter }  from './lib/reporter.mjs';
 
 export default {};
